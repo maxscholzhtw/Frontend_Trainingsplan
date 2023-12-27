2b9ddcd40d92f26e3ffde19c91f5d3f42f727a59
MarcelGudat <gudat.marcel@gmx.de>
2023-12-27 14:11:29 +0100

    Update 27.12.23 Workout erstellen im Backend

--- src/components/Create.vue
+++ src/components/Create.vue
@@ -1,24 +1,23 @@
 <template>
   <workouterstellen>
-    <form @submit.prevent="addUebung">
-
-      <button type="submit" class="btn btn-primary">Workout hinzufügen</button>
-
+    <form @submit.prevent="addWorkout">
       <div class="card">
-        <label for="beschreibung">Workoutname:</label>
-        <input id="beschreibung" v-model="newUebung.beschreibung" type="text" class="form-control" required>
-        <label for="beschreibung">Übung 1:</label>
-        <input id="beschreibung" v-model="newUebung.beschreibung" type="text" class="form-control" required>
-        <label for="beschreibung">Übung 2:</label>
-        <input id="beschreibung" v-model="newUebung.beschreibung" type="text" class="form-control" required>
-        <label for="beschreibung">Übung 3:</label>
-        <input id="beschreibung" v-model="newUebung.beschreibung" type="text" class="form-control" required>
-        <label for="beschreibung">Übung 4:</label>
-        <input id="beschreibung" v-model="newUebung.beschreibung" type="text" class="form-control" required>
-        <label for="beschreibung">Übung 5:</label>
-        <input id="beschreibung" v-model="newUebung.beschreibung" type="text" class="form-control" required>
-        <label for="beschreibung">Übung 6:</label>
-        <input id="beschreibung" v-model="newUebung.beschreibung" type="text" class="form-control" required>
+        <label for="workoutname">Workoutname:</label>
+        <input id="workoutname" v-model="newWorkout.workoutname" type="text" class="form-control" required>
+        <label for="uebung1">Übung 1:</label>
+        <input id="uebung1" v-model="newWorkout.uebung1" type="text" class="form-control" required>
+        <label for="uebung2">Übung 2:</label>
+        <input id="uebung2" v-model="newWorkout.uebung2" type="text" class="form-control" required>
+        <label for="uebung3">Übung 3:</label>
+        <input id="uebung3" v-model="newWorkout.uebung3" type="text" class="form-control" required>
+        <label for="uebung4">Übung 4:</label>
+        <input id="uebung4" v-model="newWorkout.uebung4" type="text" class="form-control" required>
+        <label for="uebung5">Übung 5:</label>
+        <input id="uebung5" v-model="newWorkout.uebung5" type="text" class="form-control" required>
+        <label for="uebung6">Übung 6:</label>
+        <input id="uebung6" v-model="newWorkout.uebung6" type="text" class="form-control" required>
+
+        <button type="submit" class="btn btn-primary">Workout hinzufügen</button>
       </div>
 
     </form>
@@ -29,20 +28,31 @@
 export default {
   data() {
     return {
-      newUebung: {
-        name: '',
-        beschreibung: '',
+      newWorkout: {
+        Workoutname: '',
+        uebung1: '',
+        uebung2: '',
+        uebung3: '',
+        uebung4: '',
+        uebung5: '',
+        uebung6: '',
+
       },
     };
   },
   methods: {
-    addUebung() {
-      console.log("Neue Uebung")
+    addWorkout() {
+      console.log("Neues Workout")
       var myHeaders = new Headers();
       myHeaders.append("Content-Type", "application/json");
       var raw = JSON.stringify({
-        "name": this.newUebung.name,
-        "beschreibung": this.newUebung.beschreibung
+        "Workoutname": this.newWorkout.Workoutname,
+        "uebung1": this.newWorkout.uebung1,
+        "uebung2": this.newWorkout.uebung2,
+        "uebung3": this.newWorkout.uebung3,
+        "uebung4": this.newWorkout.uebung4,
+        "uebung5": this.newWorkout.uebung5,
+        "uebung6": this.newWorkout.uebung6,
       });
 
       var requestOptions = {
@@ -52,7 +62,7 @@ export default {
         redirect: 'follow'
       };
 
-      fetch("http://localhost:8080/uebung", requestOptions)
+      fetch("http://localhost:8080/workout", requestOptions)
           .then(response => response.text())
           .then(result => console.log(result))
           .catch(error => console.log('error', error));
@@ -76,5 +86,10 @@ export default {
 label{
   padding: 10px 0px 10px 0px;
   margin: 10px 0px 0px 0px;
+}
+
+button{
+  margin: 10px 0px 0px 0px;
+  background-color: white;
 }
 </style>
